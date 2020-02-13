9c71d0086e8f6e62d33f6c3db1d9b3db2b6c0ed6
dannamachi <serenita15@gmail.com>
2020-02-13 15:53:15 +0700

    Add BelowStatus condition

--- BlueHeaven/src/_data/implementations/Story/SentimentCondition.cs
+++ BlueHeaven/src/_data/implementations/Story/SentimentCondition.cs
@@ -1,24 +1,36 @@
 using BlueHeaven.src.Data;
+using BlueHeaven.src.Enums;
+
 namespace BlueHeaven.src.Data.Story
 {
     // data: sentiment-based condition for IConversation
     public class SentimentCondition : IProvideCondition
     {
-        private string _character;
-        private SentimentStatus _status;
-        private string _towardsCharacter;
-        public SentimentCondition(string character, SentimentStatus hasSentiment, string towardsCharacter)
+        private CharacterCode _character;
+        private int _status;
+        private bool _moreThan;
+        private CharacterCode _towardsCharacter;
+        public SentimentCondition(CharacterCode character, SentimentStatus hasSentiment, CharacterCode towardsCharacter, bool moreThan = true)
+        {
+            _character = character;
+            _status = (int)hasSentiment;
+            _towardsCharacter = towardsCharacter;
+            _moreThan = moreThan;
+        }
+        public SentimentCondition(CharacterCode character, int hasSentiment, CharacterCode towardsCharacter, bool moreThan = true)
         {
             _character = character;
             _status = hasSentiment;
             _towardsCharacter = towardsCharacter;
+            _moreThan = moreThan;
         }
         public bool IsFulfilledBy(IGameState gameState)
         {
             ICharacter character = gameState.GetCharacter(_character);
             if (character == null) return false;
             if (!character.HasSentimentTowards(_towardsCharacter)) return false;
-            return character.SentimentTowards(_towardsCharacter).IsOfStatus(_status);
+            if (_moreThan) return character.SentimentTowards(_towardsCharacter).IsOfStatus(_status);
+            else return character.SentimentTowards(_towardsCharacter).IsBelowStatus(_status);
         }
     }
 }
